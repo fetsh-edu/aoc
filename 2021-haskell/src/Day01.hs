c5b52462eb6e107aac5f652999d4133941c84c4e
fetsh-edu <fetsh.me@gmail.com>
2021-12-01 11:56:01 +0300

    Initial take

--- 2021-haskell/src/Day01.hs
+++ 2021-haskell/src/Day01.hs
@@ -1,12 +1,25 @@
 module Day01 (solve1, solve2) where
- 
-import Relude (Text)
-import qualified Data.Text as Text
-  
-solve1 :: String -> Text
+
+import Relude ((&))
+
+import Control.Applicative
+import Data.List (tails)
+
+solve1 :: String -> String
 solve1 input =
-    Text.reverse (Text.pack input)
-    
-solve2 :: String -> Text
+    zipWith (-) (tail nums) nums & filter (> 0) & length & show
+    where
+        nums = ints input
+
+solve2 :: String -> String
 solve2 input =
-    "output2"
+    zipWith (-) (tail nums) nums & filter (> 0) & length & show
+        where
+            nums = zip3 (tail $ tail measurements) (tail measurements) measurements & map (\(a,b,c) -> a + b + c)
+            measurements = ints input
+            
+ints :: String -> [Int]
+ints = map read . lines
+
+windows :: Int -> [a] -> [[a]]
+windows m = getZipList . traverse ZipList . take m . tails
