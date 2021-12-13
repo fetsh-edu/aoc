f3cc79e6bb5fffa109054083eab28c3edf26bc4a
fetsh-edu <fetsh.me@gmail.com>
2021-12-13 12:35:34 +0300

    Day 13

--- 2021-haskell/src/Day13.hs
+++ 2021-haskell/src/Day13.hs
@@ -1,11 +1,31 @@
 module Day13 (solve1, solve2, parse) where
-  
-parse :: String -> a
-parse = undefined
 
-solve1 :: a -> Int
-solve1 _ = undefined
+import qualified Data.Set as S (Set, fromList, size, map, member)
+import AOC.Utils (splitOn, both)
+import Data.Matrix (matrix, Matrix)
+data Instruction = X Int | Y Int
 
+parse :: String -> (S.Set (Int, Int), [Instruction])
+parse input = (points, map parseInstruction instructions')
+    where
+      (points', instructions') = splitOn [""] $ lines input
+      points = S.fromList (map (both read . splitOn ",") points')
+      parseInstruction ('f':'o':'l':'d':' ':'a':'l':'o':'n':'g':' ':'y':'=':y) = Y (read y)
+      parseInstruction ('f':'o':'l':'d':' ':'a':'l':'o':'n':'g':' ':'x':'=':x) = X (read x)
 
-solve2 :: a -> Int
-solve2 _ = undefined
+solve1 :: (S.Set (Int, Int), [Instruction]) -> Int
+solve1 (pSet, instructions) =
+     S.size $ scanl (\ps ins -> S.map (applyInstruction ins) ps) pSet instructions !! 1
+
+solve2 :: (S.Set (Int, Int), [Instruction]) -> Matrix Char
+solve2 (pSet, instructions) = matrix'
+    where final = foldl (\ps ins -> S.map (applyInstruction ins) ps) pSet instructions
+          rows = (+1) $ maximum $ S.map snd final
+          cols = (+1) $ maximum $ S.map fst final
+          matrix' = matrix rows cols $ \(j,i) -> if S.member (i-1,j-1) final then '#' else ' '
+
+---------------------
+
+applyInstruction :: Instruction -> (Int, Int) -> (Int, Int)
+applyInstruction (Y int) (x, y) = if y < int then (x, y) else (x, int - (y - int))
+applyInstruction (X int) (x, y) = if x < int then (x, y) else (int - (x - int), y)
--- 2021-haskell/test/Day13Spec.hs
+++ 2021-haskell/test/Day13Spec.hs
@@ -5,12 +5,32 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
-    describe "Day 6" $ do
+    describe "" $ do
 
-      let input = "3,4,3,1,2"
+      let input = "6,10\n\
+                   \0,14\n\
+                   \9,10\n\
+                   \0,3\n\
+                   \10,4\n\
+                   \4,11\n\
+                   \6,0\n\
+                   \6,12\n\
+                   \4,1\n\
+                   \0,13\n\
+                   \10,12\n\
+                   \3,4\n\
+                   \3,0\n\
+                   \8,4\n\
+                   \1,10\n\
+                   \2,14\n\
+                   \8,10\n\
+                   \9,0\n\
+                   \\n\
+                   \fold along y=7\n\
+                   \fold along x=5\n"
 
-      it "solves Day 6 Part 1" $ do
-        Day13.solve1 (Day13.parse input) `shouldBe` 5934
+      it "Part 1" $ do
+        Day13.solve1 (Day13.parse input) `shouldBe` 17
     
-      it "solves Day 6 Part 2" $ do
-        Day13.solve2 (Day13.parse input) `shouldBe` 26984457539
+--      it "Part 2" $ do
+--        Day13.solve2 (Day13.parse input) `shouldBe` 26984457539
