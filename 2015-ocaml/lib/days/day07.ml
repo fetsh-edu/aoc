98ff4ffac93261192ab7b9dea62e50aca392a30a
fetsh-edu <fetsh.me@gmail.com>
2025-12-21 13:55:08 +0200

    2015-Day07: Rename

--- 2015-ocaml/lib/days/day07.ml
+++ 2015-ocaml/lib/days/day07.ml
@@ -1,16 +1,16 @@
-(** Day 7: TODO - Add puzzle title *)
+(** Day 7: Some Assembly Required *)
 
 open Helpers
 
-type operand = Const of int | Wire of string
+type op = Const of int | Wire of string
 
 type instr =
-  | Assign of operand
-  | Not of operand
-  | And of operand * operand
-  | Or of operand * operand
-  | LShift of operand * int
-  | RShift of operand * int
+  | Assign of op
+  | Not of op
+  | And of op * op
+  | Or of op * op
+  | LShift of op * int
+  | RShift of op * int
 
 module StringMap = Map.Make (String)
 module StringTable = Hashtbl.Make (String)
@@ -23,21 +23,19 @@ let mask16 x = x land 0xFFFF
 (** Parse the input string into your input type. The input parameter contains
     the entire puzzle input as a string. *)
 let parse_input (input : string) : t =
-  let parse_operand s =
+  let parse_op s =
     match int_of_string_opt s with Some n -> Const n | None -> Wire s
   in
   let parse_line line =
     match String.split_on_char ' ' line with
-    | [ op; "->"; dst ] -> (dst, Assign (parse_operand op))
-    | [ "NOT"; op; "->"; dst ] -> (dst, Not (parse_operand op))
-    | [ op1; "AND"; op2; "->"; dst ] ->
-        (dst, And (parse_operand op1, parse_operand op2))
-    | [ op1; "OR"; op2; "->"; dst ] ->
-        (dst, Or (parse_operand op1, parse_operand op2))
+    | [ op; "->"; dst ] -> (dst, Assign (parse_op op))
+    | [ "NOT"; op; "->"; dst ] -> (dst, Not (parse_op op))
+    | [ op1; "AND"; op2; "->"; dst ] -> (dst, And (parse_op op1, parse_op op2))
+    | [ op1; "OR"; op2; "->"; dst ] -> (dst, Or (parse_op op1, parse_op op2))
     | [ op; "LSHIFT"; n; "->"; dst ] ->
-        (dst, LShift (parse_operand op, int_of_string n))
+        (dst, LShift (parse_op op, int_of_string n))
     | [ op; "RSHIFT"; n; "->"; dst ] ->
-        (dst, RShift (parse_operand op, int_of_string n))
+        (dst, RShift (parse_op op, int_of_string n))
     | _ -> failwith ("Invalid line: " ^ line)
   in
   input |> String.split_on_char '\n'
@@ -49,7 +47,7 @@ let parse_input (input : string) : t =
            StringMap.add dest ins acc)
        StringMap.empty
 
-let rec eval_wire (circuit : circuit) (memo : int StringTable.t) wire =
+let rec eval_wire circuit memo wire =
   match StringTable.find_opt memo wire with
   | Some value -> value
   | None -> (
@@ -60,27 +58,17 @@ let rec eval_wire (circuit : circuit) (memo : int StringTable.t) wire =
           StringTable.add memo wire value;
           value)
 
-and eval_instruction (circuit : circuit) (memo : int StringTable.t) = function
-  | Assign op -> eval_operand circuit memo op |> mask16
-  | Not op ->
-      let x = eval_operand circuit memo op in
-      mask16 (lnot x)
+and eval_instruction circuit memo = function
+  | Assign op -> eval_op circuit memo op |> mask16
+  | Not op -> lnot (eval_op circuit memo op) |> mask16
   | And (op1, op2) ->
-      let x = eval_operand circuit memo op1 in
-      let y = eval_operand circuit memo op2 in
-      mask16 (x land y)
+      eval_op circuit memo op1 land eval_op circuit memo op2 |> mask16
   | Or (op1, op2) ->
-      let x = eval_operand circuit memo op1 in
-      let y = eval_operand circuit memo op2 in
-      mask16 (x lor y)
-  | LShift (op1, n) ->
-      let x = eval_operand circuit memo op1 in
-      mask16 (x lsl n)
-  | RShift (op1, n) ->
-      let x = eval_operand circuit memo op1 in
-      mask16 (x lsr n)
+      eval_op circuit memo op1 lor eval_op circuit memo op2 |> mask16
+  | LShift (op1, n) -> eval_op circuit memo op1 lsl n |> mask16
+  | RShift (op1, n) -> eval_op circuit memo op1 lsr n |> mask16
 
-and eval_operand (circuit : circuit) (memo : int StringTable.t) = function
+and eval_op circuit memo = function
   | Const n -> n
   | Wire w -> eval_wire circuit memo w
 
